b318f719ab5ac563404ea6d43467ec4992eb93fd
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-01-24 14:55:28 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let textbox = document.getElementById("evaluatedText");
+let H3=document.getElementById("wordCount");
+
+textbox.addEventListener("input", text)
+
+function text(){
+    let word=textbox.value;
+    let wordlength=word.length;
+    wordCount.innerHTML= wordlength;
+}
+
